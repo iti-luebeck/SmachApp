5e8a3eb1645a39d2a451d9061b842689dbfc98ee
Morten Mey <mey@informatik.uni-luebeck.de>
2014-04-19 12:48:28 +0200

    Initial Project Creation

---
+++ app/src/main/java/de/uni_luebeck/iti/smachapp/app/MainActivity.java
@@ -0,0 +1,20 @@
+package de.uni_luebeck.iti.smachapp.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void newFSM(View view){
+
+    }
+
+}
